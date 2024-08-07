0963fc36e91c6fa8bb1b483e5a70529ec09e5857
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-07 16:37:37 +0000

    Category validation is added.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,7 @@ class Category < ApplicationRecord
   enum category_type: { income: 0, expense: 1 }
 
   validates :name, presence: true
+  validates :name, uniqueness: { scope: :user }
 
   belongs_to :user
   has_many :user_transactions, dependent: :nullify
