50fde3d0399b979154e913499aeb723cd3676629
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-08 18:55:05 +0000

    account create service is added.

---
+++ app/operations/api/v0/accounts/create.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Api::V0::Accounts
+  class Create
+    include ApplicationService
+
+    class Contract < ApplicationContract
+      params do
+        required(:name).filled(:string)
+        optional(:initial_balance_cents).maybe(:integer)
+      end
+    end
+
+    def execute(params, current_user:)
+      @params = params
+      @current_user = current_user
+
+      yield create_account
+      Success(json_serialize)
+    end
+
+    private
+
+    attr_reader :params, :current_user, :account
+
+    def create_account
+      initial_balance_cents = params[:initial_balance_cents] || 0
+      @account = Account.new(name: params[:name], initial_balance_cents:, user: current_user)
+
+      return Success(account) if account.save
+
+      Failure(account.errors.full_messages)
+    end
+
+    def json_serialize
+      Api::V0::AccountsSerializer.render_as_hash(account)
+    end
+  end
+end
