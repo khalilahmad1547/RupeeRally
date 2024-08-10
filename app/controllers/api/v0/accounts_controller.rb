248b78e1d66fecec695dd4a159a89d7bc8d6333c
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-10 18:15:34 +0000

    show action is added.

--- app/controllers/api/v0/accounts_controller.rb
+++ app/controllers/api/v0/accounts_controller.rb
@@ -9,6 +9,14 @@ module Api::V0
       end
     end
 
+    def show
+      Accounts::Show.call(params.to_unsafe_h, current_user: @current_user) do |result|
+        result.success { |data| success_response(data, status: :created) }
+        result.failure(:not_found) { not_found_response }
+        result.failure { |errors| unprocessable_entity(errors) }
+      end
+    end
+
     def create
       Accounts::Create.call(params.to_unsafe_h, current_user: @current_user) do |result|
         result.success { |data| success_response(data, status: :created) }
