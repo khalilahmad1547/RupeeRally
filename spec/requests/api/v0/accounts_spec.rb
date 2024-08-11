99dfb34f947e145b296988374e29753570ac2995
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-11 19:03:25 +0000

    specs for GET@show account is added.

--- spec/requests/api/v0/accounts_spec.rb
+++ spec/requests/api/v0/accounts_spec.rb
@@ -52,4 +52,41 @@ RSpec.describe '/api/v0/accounts', type: :request do
       include_context 'unauthorized'
     end
   end
+  describe 'GET#show' do
+    let(:user) { create(:user) }
+    let(:access_token) { valid_jwt(user) }
+    let(:headers) do
+      {
+        Authorization: access_token
+      }
+    end
+    let(:accounts) { create_list(:account, 100, user:) }
+    let(:selected_account) { accounts.sample }
+
+    before { get "/api/v0/accounts/#{selected_account.id}", headers: }
+
+    describe 'success' do
+      context 'when account id is valid' do
+        it 'returns account' do
+          expect(response).to be_ok
+          expect(response.parsed_body['data']['accounts'].count).to eq(1)
+          expect(response.parsed_body['data']['accounts'].first['id']).to eq(selected_account.id)
+          expect(response).to match_json_schema('v0/accounts/show')
+        end
+      end
+    end
+
+    describe 'failure' do
+      include_context 'forbidden'
+      include_context 'unauthorized'
+
+      context 'when account id is not valid' do
+        let(:selected_account) { create(:account) }
+
+        it 'returns not_found' do
+          expect(response).to be_not_found
+        end
+      end
+    end
+  end
 end
