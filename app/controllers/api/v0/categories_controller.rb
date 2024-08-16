05d0742e4b67444bde69c4103bbdd6ef4a4591a4
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-16 17:53:19 +0000

    categories controller is updated.

--- app/controllers/api/v0/categories_controller.rb
+++ app/controllers/api/v0/categories_controller.rb
@@ -4,14 +4,14 @@ module Api::V0
   class CategoriesController < ApiController
     def index
       Categories::Index.call(params.to_unsafe_h, current_user: @current_user) do |result|
-        result.success { |accounts| success_response(accounts, status: :ok) }
+        result.success { |categories| success_response(categories, status: :ok) }
         result.failure { |errors| unprocessable_entity(errors) }
       end
     end
 
     def show
       Categories::Show.call(params.to_unsafe_h, current_user: @current_user) do |result|
-        result.success { |data| success_response(data, status: :ok) }
+        result.success { |category| success_response(category, status: :ok) }
         result.failure(:not_found) { not_found_response }
         result.failure { |errors| unprocessable_entity(errors) }
       end
@@ -19,14 +19,14 @@ module Api::V0
 
     def create
       Categories::Create.call(params.to_unsafe_h, current_user: @current_user) do |result|
-        result.success { |data| success_response(data, status: :created) }
+        result.success { |category| success_response(category, status: :created) }
         result.failure { |errors| unprocessable_entity(errors) }
       end
     end
 
     def update
       Categories::Update.call(params.to_unsafe_h, current_user: @current_user) do |result|
-        result.success { |data| success_response(data, status: :ok) }
+        result.success { |category| success_response(category, status: :ok) }
         result.failure(:not_found) { not_found_response }
         result.failure { |errors| unprocessable_entity(errors) }
       end
