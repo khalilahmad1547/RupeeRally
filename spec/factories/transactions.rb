8b8853ff0e332a5b8967766cd0bae98ad190b5d8
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-21 17:09:51 +0000

    transaction factory is updated.

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :transaction do
-    description { 'my expense' }
+    description { Faker::Lorem.sentence }
     amount_cents { Faker::Number.number(digits: 5) }
-    divided_by { 1 }
+    divided_by { Transaction.divided_bies.keys.sample }
+    selected_date { Date.today.strftime(Constants::API_DATE_FROMAT) }
+    selected_time { Time.now.strftime(Constants::API_TIME_FROMAT) }
+    user { create(:user) }
   end
 end
