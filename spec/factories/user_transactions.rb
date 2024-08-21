82fe0cbee3425361a01300e1170c984223086781
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-21 17:10:08 +0000

    user_transaction is updated.

--- spec/factories/user_transactions.rb
+++ spec/factories/user_transactions.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :user_transaction do
-    description { 'my expense' }
-    transaction_type { 1 }
+    description { Faker::Lorem.sentence }
+    transaction_type { UserTransaction.transaction_types.keys.sample }
     user_share { 1 }
     amount_cents { Faker::Number.number(digits: 5) }
     status { 0 }
+    account { create(:account) }
+    category { create(:category) }
+    parent_transaction { create(:transaction) }
   end
 end
