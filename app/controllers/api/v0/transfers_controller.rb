f49c11f511a03363407b4f40d5fa916ca6068640
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-22 19:30:47 +0000

    create action is added.

---
+++ app/controllers/api/v0/transfers_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Api::V0
+  class TransfersController < ApiController
+    def show; end
+
+    def create
+      Transfers::Create.call(params.to_unsafe_h, current_user: @current_user) do |result|
+        result.success { |transaction| success_response(transaction, status: :created) }
+        result.failure(:from_account_not_found) { not_found_response('From account not found') }
+        result.failure(:to_account_not_found) { not_found_response('To account not found') }
+        result.failure { |errors| unprocessable_entity(errors) }
+      end
+    end
+
+    def update; end
+
+    def destroy; end
+  end
+end
