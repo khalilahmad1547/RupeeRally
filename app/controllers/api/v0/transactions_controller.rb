6fac2b729ad619e49bddd9d1fa953c45508b1283
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-22 15:56:56 +0000

    corrected not found errors.
    * before it was not handling account, category not found error
    * now :not_found will handle transaction not found error

--- app/controllers/api/v0/transactions_controller.rb
+++ app/controllers/api/v0/transactions_controller.rb
@@ -30,6 +30,8 @@ module Api::V0
       Transactions::Update.call(params.to_unsafe_h, current_user: @current_user) do |result|
         result.success { |transaction| success_response(transaction, status: :ok) }
         result.failure(:not_found) { not_found_response }
+        result.failure(:account_not_found) { not_found_response('Account not found') }
+        result.failure(:category_not_found) { not_found_response('Category not found') }
         result.failure { |errors| unprocessable_entity(errors) }
       end
     end
