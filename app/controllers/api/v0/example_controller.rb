ae81409d275618df2b8df796c13931711a4e0914
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-08-22 18:39:30 +0000

    deleted example controller.

--- app/controllers/api/v0/example_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Api::V0
-  class ExampleController < ApiController
-    def index
-      render json: { status: :ok, user: @current_user }, status: :ok
-    end
-  end
-end
