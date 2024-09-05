6d6034a036f373092a5d1af2ac3d8f73b2eef115
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-09-05 15:34:00 +0000

    routes are updated.

--- config/routes/api_v0.rb
+++ config/routes/api_v0.rb
@@ -14,7 +14,8 @@ namespace :api do
 
     resources :accounts, only: api_only_routes
     resources :categories, only: api_only_routes
-    resources :transactions, only: api_only_routes
-    resources :transfers, only: %i[create update destroy]
+    resources :transactions, only: %i[index show destroy]
+    resources :transfer_transactions, only: %i[create update]
+    resources :individual_transactions, only: %i[create update]
   end
 end
